7e4d5c70a3a373a2e5044d5d6efa94d98b2e3b6c
Gustavo Antonio Correa Caja <gcorreageek@gmail.com>
2013-05-03 19:48:51 -0500

    primer commit!

---
+++ PAlmacenes/src/com/prueba/Prueba.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.prueba;
+
+/**
+ * @author proynantec04
+ *
+ */
+public class Prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
